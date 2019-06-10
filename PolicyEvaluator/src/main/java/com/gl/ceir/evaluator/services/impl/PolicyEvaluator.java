529b48d7e466e3f54f27490ed1fe3214f7c0aeba
amehta <amehta@DT00648>
2019-06-10 17:57:48 +0530

    Policy Evaluator

--- PolicyEvaluator/src/main/java/com/gl/ceir/evaluator/services/impl/PolicyEvaluator.java
+++ PolicyEvaluator/src/main/java/com/gl/ceir/evaluator/services/impl/PolicyEvaluator.java
@@ -3,10 +3,13 @@ package com.gl.ceir.evaluator.services.impl;
 import java.util.List;
 import java.util.concurrent.Executor;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.gl.ceir.config.controller.BlackListController;
 import com.gl.ceir.config.model.Rules;
 import com.gl.ceir.config.model.constants.ActionNames;
 import com.gl.ceir.config.system.request.Request;
@@ -18,6 +21,7 @@ import com.gl.ceir.evaluator.services.RuleSolverService;
 @Component
 @Scope("PROTOTYPE")
 public class PolicyEvaluator {
+	private static final Logger logger = LogManager.getLogger(PolicyEvaluator.class);
 
 	@Autowired
 	private InputRepository inputRepository;
@@ -32,18 +36,31 @@ public class PolicyEvaluator {
 	public PolicyEvaluator() {
 	}
 
+	private boolean start;
+
+	public void setStart(boolean start) {
+		this.start = start;
+	}
+
 	public void run() {
 		while (true) {
-			List<Request> requests = inputRepository.read();
+			try {
+				while (start) {
+					List<Request> requests = inputRepository.read();
 
-			if (requests == null) {
-				continue;
-			} else {
-				for (Request request : requests) {
-					executor.execute(() -> {
-						startPolicyEvaluator.process(request);
-					});
+					if (requests == null) {
+						continue;
+					} else {
+						start = false;
+						for (Request request : requests) {
+							executor.execute(() -> {
+								startPolicyEvaluator.process(request);
+							});
+						}
+					}
 				}
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
 			}
 		}
 	}
