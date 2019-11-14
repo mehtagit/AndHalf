b45d7dfa582d264d9139110a5a08c3f975109908
pc <pc@pc-PC>
2019-11-14 19:46:47 +0530

    Resolved issue with StateMgmtRepository.

--- config/src/main/java/com/gl/ceir/config/repository/StateMgmtRepository.java
+++ config/src/main/java/com/gl/ceir/config/repository/StateMgmtRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gl.ceir.config.model.StateMgmtDb;
@@ -12,7 +11,6 @@ import com.gl.ceir.config.model.StateMgmtDb;
 @Repository
 public interface StateMgmtRepository extends JpaRepository<StateMgmtDb, Long>, JpaSpecificationExecutor<StateMgmtDb> {
 
-	@Query("select s from state_mgmt_db inner join fetch s.feature_id states_interpretation_db ")
 	public List<StateMgmtDb> getByFeatureIdAndUserTypeId(Integer featureId, Integer userTypeId);
 
 }
