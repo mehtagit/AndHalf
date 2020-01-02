5aa51d88bbb315b959636920666efaa62ef78920
user <user@user-PC.Dlink>
2020-01-02 16:34:47 +0530

    BlackProcess

---
+++ BlackListProcess/src/main/java/cm/ceir/BlackListProcess/App.java
@@ -0,0 +1,13 @@
+package cm.ceir.BlackListProcess;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
