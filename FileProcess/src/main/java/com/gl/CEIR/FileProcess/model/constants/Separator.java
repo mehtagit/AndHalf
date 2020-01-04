b0806d56af10c47e25bc38eee7de8295b770fc38
pc <pc@pc-PC>
2020-01-04 17:08:04 +0530

    Commit

--- FileProcess/src/main/java/com/gl/CEIR/FileProcess/model/constants/Separator.java
+++ FileProcess/src/main/java/com/gl/CEIR/FileProcess/model/constants/Separator.java
@@ -2,8 +2,8 @@ package com.gl.CEIR.FileProcess.model.constants;
 
 public interface Separator {
 	
-	public final static String SLASH = "/";
-	public final static String COMMA = ",";
-	public final static String NEW_LINE = "\n";
+	public String SLASH = "/";
+	public String COMMA = ",";
+	public String NEW_LINE = "\n";
 
 }
