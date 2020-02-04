1614668dfbc2f2ba5aed15ef5319dfa4f9bf6e1f
pc <pc@pc-PC>
2020-02-04 15:02:02 +0530

    Fix : Failed test cases regarding end user message.

--- config/src/main/java/com/gl/ceir/config/controller/VisaController.java
+++ config/src/main/java/com/gl/ceir/config/controller/VisaController.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
