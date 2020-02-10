182aff05ea60081374cfb278eae6a884165f5902
user <user@user-PC>
2020-02-10 18:20:44 +0530

    audit trial on check device

--- config/src/main/java/com/gl/ceir/config/service/impl/CheckDeviceImpl.java
+++ config/src/main/java/com/gl/ceir/config/service/impl/CheckDeviceImpl.java
@@ -1,17 +1,25 @@
 package com.gl.ceir.config.service.impl;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.gl.ceir.config.model.AuditTrail;
 import com.gl.ceir.config.model.CheckDevice;
 import com.gl.ceir.config.model.CheckDeviceReponse;
 import com.gl.ceir.config.model.GenricResponse;
+import com.gl.ceir.config.model.constants.Features;
+import com.gl.ceir.config.model.constants.SubFeatures;
+import com.gl.ceir.config.repository.AuditTrailRepository;
 @Service
 public class CheckDeviceImpl {
 
+	@Autowired
+	AuditTrailRepository auditTrailRepository;
+	
 	public GenricResponse checkDevices( CheckDevice checkDevice) {
 		   CheckDeviceReponse checkDeviceResponse=new CheckDeviceReponse();
+			auditTrailRepository.save(new AuditTrail(0, null, 0L, "", 0L,"Check Device", "Check Device", ""));
 	        checkDeviceResponse.setBrandName("Redmi");
 	        checkDeviceResponse.setModelName("Ac11");
 	        checkDeviceResponse.setTacNumber("476433753");
