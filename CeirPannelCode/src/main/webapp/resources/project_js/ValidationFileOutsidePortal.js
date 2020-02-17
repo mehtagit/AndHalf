f11a6acf3d031a114ed26b45d6130c5f58f54b0f
ADISH ANSARI <ADISH ANSARI@DESKTOP-6SA8CKL>
2020-02-17 13:32:59 +0530

    commit : Blank

---
+++ CeirPannelCode/src/main/webapp/resources/project_js/ValidationFileOutsidePortal.js
@@ -0,0 +1,42 @@
+
+function InvalidMsg(textbox,type) {
+    if (textbox.value == '') {
+    	if(type=="input"){
+        textbox.setCustomValidity($.i18n('requiredMsg_input'));
+    	}
+    	else if(type=="date"){
+            textbox.setCustomValidity($.i18n('requiredMsg_date'));	
+    	}
+    	else if(type=="select"){
+            textbox.setCustomValidity($.i18n('requiredMsg_select'));	
+    	}
+    	else if(type=="fileType"){
+            textbox.setCustomValidity($.i18n('requiredMsg_fileType'));	
+    	}
+    	else if(type=="email"){
+            textbox.setCustomValidity($.i18n('requiredMsg_email'));	
+    	}
+    	
+    }
+  /*  else if(textbox.validity.typeMismatch){
+        
+        if(type=="input"){
+        	textbox.setCustomValidity($.i18n('requiredMsg_input'));
+        	}
+        	else if(type=="date"){
+        		textbox.setCustomValidity($.i18n('requiredMsg_date'));	
+        	}
+        	else if(type=="select"){
+        		textbox.setCustomValidity($.i18n('requiredMsg_select'));	
+        	}
+        	else if(type=="fileType"){
+        		textbox.setCustomValidity($.i18n('requiredMsg_fileType'));
+        	}
+        	
+        
+    }*/
+    else {
+        textbox.setCustomValidity('');
+    }
+    return true;
+}
