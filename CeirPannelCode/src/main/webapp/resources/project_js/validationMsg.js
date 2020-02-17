fbb7a2925eca66c48962b2daa80335e9fe5182ac
pc <pc@pc-PC>
2020-02-17 13:29:10 +0530

    commit:-validation

---
+++ CeirPannelCode/src/main/webapp/resources/project_js/validationMsg.js
@@ -0,0 +1,52 @@
+var lang=window.parent.$('#langlist').val() == 'km' ? 'km' : 'en';
+
+
+		$.i18n().locale = lang;	
+		
+		$.i18n().load( {
+			'en': '../resources/i18n/en.json',
+			'km': '../resources/i18n/km.json'
+		} ).done( function() { 
+		});
+function InvalidMsg(textbox,type) {
+    if (textbox.value == '') {
+    	if(type=="input"){
+        textbox.setCustomValidity($.i18n('requiredMsg_input'));
+    	}
+    	else if(type=="date"){
+            textbox.setCustomValidity($.i18n('requiredMsg_date'));	
+    	}
+    	else if(type=="select"){
+            textbox.setCustomValidity($.i18n('requiredMsg_select'));	
+    	}
+    	else if(type=="fileType"){
+            textbox.setCustomValidity($.i18n('requiredMsg_fileType'));	
+    	}
+    	else if(type=="email"){
+            textbox.setCustomValidity($.i18n('requiredMsg_email'));	
+    	}
+    	
+    	
+    }
+  /*  else if(textbox.validity.typeMismatch){
+        
+        if(type=="input"){
+        	textbox.setCustomValidity($.i18n('requiredMsg_input'));
+        	}
+        	else if(type=="date"){
+        		textbox.setCustomValidity($.i18n('requiredMsg_date'));	
+        	}
+        	else if(type=="select"){
+        		textbox.setCustomValidity($.i18n('requiredMsg_select'));	
+        	}
+        	else if(type=="fileType"){
+        		textbox.setCustomValidity($.i18n('requiredMsg_fileType'));
+        	}
+        	
+        
+    }*/
+    else {
+        textbox.setCustomValidity('');
+    }
+    return true;
+}
