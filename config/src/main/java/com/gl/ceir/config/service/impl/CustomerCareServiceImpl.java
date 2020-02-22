317749e7b940d67e0e5a0312ffa8031eef540797
pc <pc@192.168.1.205>
2020-02-23 01:51:44 +0530

    commit;

--- config/src/main/java/com/gl/ceir/config/service/impl/CustomerCareServiceImpl.java
+++ config/src/main/java/com/gl/ceir/config/service/impl/CustomerCareServiceImpl.java
@@ -43,7 +43,7 @@ public class CustomerCareServiceImpl {
 	public GenricResponse getAll(CustomerCareRequest customerCareRequest) {
 		try {
 			List<CustomerCareDeviceState> customerCareDeviceStates = new LinkedList<>();
-			
+
 			if(Objects.nonNull(customerCareRequest.getImei()) 
 					&& "IMEI".equalsIgnoreCase(customerCareRequest.getDeviceIdType())) {
 
@@ -53,18 +53,24 @@ public class CustomerCareServiceImpl {
 						logger.info("Corresponding object of Db [" + o + "] is not defined in the factory ");
 						return;
 					}
-					customerCareDeviceStates.add(customerCareTarget.fetchDetailsByImei(customerCareRequest.getImei()));
+					
+					// To avoid 500, if only one or few DB's have issue for the imei.
+					try {
+						customerCareDeviceStates.add(customerCareTarget.fetchDetailsByImei(customerCareRequest.getImei()));
+					}catch (Exception e) {
+						logger.error("Db [" + o + "] have some issue in fetching data for imei [" + customerCareRequest.getImei() + "] ");
+					}
 				});
-				
+
 				return new GenricResponse(0, GenericMessageTags.SUCCESS.getMessage(), "", customerCareDeviceStates);
 			}else if(Objects.nonNull(customerCareRequest.getMsisdn())){
 				// TODO
-				
+
 				return new GenricResponse(0, GenericMessageTags.SUCCESS.getMessage(), "", null);
 			}else {
 				return new GenricResponse(1, GenericMessageTags.INVALID_REQUEST.getMessage(), "", null);
 			}
-			
+
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			throw new ResourceServicesException(this.getClass().getName(), e.getMessage());
