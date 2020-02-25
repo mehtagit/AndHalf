a2b3bc671af0790eb63f8a3a5d17e8243ff5bf8c
pc <pc@pc-PC>
2020-02-26 00:49:37 +0530

    Added device usage db entity with its repository.

---
+++ config/src/main/java/com/gl/ceir/config/repository/DeviceUsageDbRepository.java
@@ -0,0 +1,13 @@
+package com.gl.ceir.config.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.gl.ceir.config.model.DeviceUsageDb;
+
+@Repository
+public interface DeviceUsageDbRepository extends JpaRepository<DeviceUsageDb, Long>, 
+JpaSpecificationExecutor<DeviceUsageDb> {
+
+}
