87c4df7a07f6ddd57ffda49f06bae1b90f498c4a
pc <pc@192.168.1.205>
2020-03-25 19:05:17 +0530

    Commit;

---
+++ CEIRCdrParserM/src/com/functionapps/dao/DeviceCustomDbDao.java
@@ -0,0 +1,142 @@
+package com.functionapps.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.log4j.Logger;
+
+import com.functionapps.pojo.DeviceImporterDb;
+
+public class DeviceCustomDbDao {
+	static Logger logger = Logger.getLogger(DeviceCustomDbDao.class);
+	
+	public void insertDeviceCustomDbAud(Connection conn, List<DeviceImporterDb> deviceImporterDbs) {
+		String query = "";
+		Statement stmt = null;
+		
+		try {
+			stmt = conn.createStatement();
+
+			for (DeviceImporterDb deviceImporterDb : deviceImporterDbs) {
+				query = "insert into device_importer_aud (rev, revtype, created_on, device_action, device_id_type, "
+						+ "device_launch_date, device_status," 
+						+ "device_type, imei_esn_meid, importer_date, importer_device_status, importer_txn_id,"
+						+ "manufature_date, modified_on, multiple_sim_status, period," 
+						+ "sn_of_device, device_block_status, local_date, previous_device_status," 
+						+ "txn_id, user_id, device_state, feature_id"
+						+ ") values("
+						+ deviceImporterDb.getId() + "," + 2 + ",'" + deviceImporterDb.getCreatedOn() 
+						+ "','" + deviceImporterDb.getDeviceAction() + "','" + deviceImporterDb.getDeviceIdType() + "','" 
+						+ deviceImporterDb.getDeviceLaunchDate() + "','" + deviceImporterDb.getDeviceStatus() + "','" 
+						+ deviceImporterDb.getDeviceType() + "','" + deviceImporterDb.getImeiEsnMeid() + "','" 
+						+ deviceImporterDb.getManufatureDate() + "','" + deviceImporterDb.getModifiedOn() + "','"
+						+ deviceImporterDb.getMultipleSimStatus() + "','" + deviceImporterDb.getPeriod() + "','"
+						+ deviceImporterDb.getSnOfDevice() + "','" + deviceImporterDb.getDeviceStatus() + "','"
+						+ deviceImporterDb.getLocalDate() + "','" + deviceImporterDb.getPreviousDeviceStatus() + "','"
+						+ deviceImporterDb.getTxnId() + "'," + deviceImporterDb.getUserId() + ","
+						+ deviceImporterDb.getDeviceStatus() + "," + deviceImporterDb.getFeatureId()
+						+")";
+
+				System.out.println("Add device_importer_aud [" + query + " ]");
+				logger.info("Add feature file Details in config DB["+query+"]");
+				stmt.addBatch(query);
+			}
+
+			stmt.executeBatch();
+			conn.commit();
+
+		} catch (SQLException e) {
+			logger.error(e.getMessage(), e);
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				if(Objects.nonNull(stmt))
+					stmt.close();
+			} catch (SQLException e) {
+				logger.error(e.getMessage(), e);
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void insertDeviceCustomDb(Connection conn, List<DeviceImporterDb> deviceImporterDbs) {
+		String query = "";
+		Statement stmt = null;
+		
+		try {
+			stmt = conn.createStatement();
+
+			for (DeviceImporterDb deviceImporterDb : deviceImporterDbs) {
+				query = "insert into device_custom_db (created_on, device_action, device_id_type, "
+						+ "device_launch_date, device_status," 
+						+ "device_type, imei_esn_meid, importer_date, importer_device_status, importer_txn_id,"
+						+ "manufature_date, modified_on, multiple_sim_status, period," 
+						+ "sn_of_device, device_block_status, local_date, previous_device_status," 
+						+ "txn_id, user_id, device_state, feature_id"
+						+ ") values("
+						+ deviceImporterDb.getCreatedOn() 
+						+ "','" + deviceImporterDb.getDeviceAction() + "','" + deviceImporterDb.getDeviceIdType() + "','" 
+						+ deviceImporterDb.getDeviceLaunchDate() + "','" + deviceImporterDb.getDeviceStatus() + "','" 
+						+ deviceImporterDb.getDeviceType() + "','" + deviceImporterDb.getImeiEsnMeid() + "','" 
+						+ deviceImporterDb.getManufatureDate() + "','" + deviceImporterDb.getModifiedOn() + "','"
+						+ deviceImporterDb.getMultipleSimStatus() + "','" + deviceImporterDb.getPeriod() + "','"
+						+ deviceImporterDb.getSnOfDevice() + "','" + deviceImporterDb.getDeviceStatus() + "','"
+						+ deviceImporterDb.getLocalDate() + "','" + deviceImporterDb.getPreviousDeviceStatus() + "','"
+						+ deviceImporterDb.getTxnId() + "'," + deviceImporterDb.getUserId() + ","
+						+ deviceImporterDb.getDeviceStatus() + "," + deviceImporterDb.getFeatureId()
+						+")";
+
+				System.out.println("Add device_importer_aud [" + query + " ]");
+				logger.info("Add feature file Details in config DB["+query+"]");
+				stmt.addBatch(query);
+			}
+
+			stmt.executeBatch();
+			conn.commit();
+
+		} catch (SQLException e) {
+			logger.error(e.getMessage(), e);
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				if(Objects.nonNull(stmt))
+					stmt.close();
+			} catch (SQLException e) {
+				logger.error(e.getMessage(), e);
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public int deleteDevicesFromDeviceCustomDb(Connection conn, String txnId) {
+		String query = "";
+		Statement stmt = null;
+		int executeStatus = 0;
+
+		query = "delete from device_custom_db where txn_id='" + txnId + "'";	
+		logger.info("delete device_custom_db ["+query+"]");
+		System.out.println("delete device_custom_db ["+query+"]");
+
+		try {
+			stmt = conn.createStatement();
+			executeStatus = stmt.executeUpdate(query);
+			conn.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return executeStatus;
+	}  
+	
+}
---
+++ CEIRCdrParserM/src/com/functionapps/parser/service/ApproveConsignment.java
@@ -0,0 +1,43 @@
+package com.functionapps.parser.service;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.functionapps.dao.DeviceDbDao;
+import com.functionapps.dao.DeviceImporterDbDao;
+import com.functionapps.parser.CEIRFeatureFileFunctions;
+import com.functionapps.parser.Rule;
+import com.functionapps.pojo.DeviceDb;
+import com.functionapps.pojo.DeviceImporterDb;
+
+public class ApproveConsignment {
+	static Logger logger = Logger.getLogger(ApproveConsignment.class);
+
+	public void process(Connection conn, String operator, String sub_feature, ArrayList<Rule> rulelist, String txnId, String operator_tag ){
+
+		DeviceDbDao deviceDbDao = new DeviceDbDao();
+		DeviceImporterDbDao deviceImporterDbDao = new DeviceImporterDbDao();
+		CEIRFeatureFileFunctions ceirfunction = new CEIRFeatureFileFunctions();
+		
+		try{
+			List<DeviceDb> deviceDbs = deviceDbDao.getDeviceDbByTxnId(conn, "", txnId);
+			System.out.println("deviceDbs" + deviceDbs);
+			deviceDbDao.insertDeviceDbAud(conn, deviceDbs);
+			deviceDbDao.deleteDevicesFromDeviceDb(conn, txnId);
+
+			List<DeviceImporterDb> deviceImporterDbs = deviceImporterDbDao.getDeviceImporterDbByTxnId(conn, "", txnId);
+			deviceImporterDbDao.insertDeviceImporterDbAud(conn, deviceImporterDbs);
+			deviceImporterDbDao.deleteDevicesFromDeviceImporterDb(conn, txnId);
+			ceirfunction.updateFeatureFileStatus(conn, txnId, 2, operator, sub_feature);
+			// TODO hit API to update delete flag in consignment.
+			conn.commit();
+
+		}catch(Exception e){
+			e.printStackTrace();
+			logger.error(e.getMessage(), e);
+		}
+	}
+}
