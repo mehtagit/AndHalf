3c5584f5bc9cb969d8fa8bfbe3740a18cccef613
Dharmender Manral <dharmender.manral@gmail.com>
2020-04-02 22:49:06 +0530

    commit

--- CeirPannelCode/src/main/webapp/resources/project_js/globalVariables.js
+++ CeirPannelCode/src/main/webapp/resources/project_js/globalVariables.js
@@ -1,7 +1,6 @@
 $('input').on('invalid', function(e) {
     setTimeout(function(){
-        $('html, body').animate({scrollTop: document.documentElement.scrollTop - 15}, 0);
-       // $('html, body').animate({scrollTop: document.documentElement.scrollDown}, 0);
+        $('html, body').animate({scrollTop: document.documentElement.scrollTop}, 0);
     }, 0);
 });
 
