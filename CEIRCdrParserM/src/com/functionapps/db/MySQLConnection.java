17f1e64cb4309b9b83b0a8018cf33e8683cf7f77
pc <pc@pc-PC>
2020-05-11 11:57:29 +0530

    jhgfdszb

--- CEIRCdrParserM/src/com/functionapps/db/MySQLConnection.java
+++ CEIRCdrParserM/src/com/functionapps/db/MySQLConnection.java
@@ -8,42 +8,42 @@ import javax.sql.DataSource;
 
 public class MySQLConnection {
 
-    public Connection getConnection() {
+	public Connection getConnection() {
 
-//    	TEST SERVER
-//        final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver"; 
-//	    final String DB_URL = "jdbc:oracle:thin:@172.24.1.96:1521/dbdmcrac";
-//	   final String USER = "CEIRCONFIG";
-//	   final String PASS = "CEIRCONFIG";
+		//    	TEST SERVER
+		// final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver"; 
+		// final String DB_URL = "jdbc:oracle:thin:@172.24.1.96:1521/dbdmcrac";
+		// final String USER = "CEIRCONFIG";
+		// final String PASS = "CEIRCONFIG";
 
 
 
-        // sqlplus CRESTELCEIR/CRESTELCEIR@//dmc-prod-db:1521/dmcproddb
-        // PROD SERVER
-        final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
-        final String DB_URL = "jdbc:oracle:thin:@dmc-prod-db:1521/dmcproddb";
-        final String USER = "CRESTELCEIR";
-        final String PASS = "CRESTELCEIR";
+		// sqlplus CRESTELCEIR/CRESTELCEIR@//dmc-prod-db:1521/dmcproddb
+		// PROD SERVER
+		final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+		final String DB_URL = "jdbc:oracle:thin:@dmc-prod-db:1521/dmcproddb";
+		final String USER = "CRESTELCEIR";
+		final String PASS = "CRESTELCEIR";
 
-    	//DEV SERVER
+		//DEV SERVER
 		/*
 		 * final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; final String DB_URL =
 		 * "jdbc:mysql://172.31.22.35:3306/ceirconfig"; final String USER = "root";
 		 * final String PASS = "root";
 		 */
-        
-        Connection conn = null;
-        try {
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            conn.setAutoCommit(false);
-            System.out.println("Connnection created successfully" + conn);
-            return conn;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
 
-    }
+		Connection conn = null;
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			conn.setAutoCommit(false);
+			System.out.println("Connnection created successfully" + conn);
+			return conn;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
 
 }
