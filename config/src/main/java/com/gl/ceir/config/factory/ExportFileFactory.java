1626358abc589e8db28a7d36e31fbed6a8708045
pc <pc@pc-PC>
2020-06-21 18:44:51 +0530

    Commit;

--- config/src/main/java/com/gl/ceir/config/factory/ExportFileFactory.java
+++ config/src/main/java/com/gl/ceir/config/factory/ExportFileFactory.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.gl.ceir.config.factory.impl.ExportFileStockCeirAdmin;
 import com.gl.ceir.config.factory.impl.ExportFileStockCustom;
 import com.gl.ceir.config.factory.impl.ExportFileStockDistributor;
 import com.gl.ceir.config.factory.impl.ExportFileStockImporter;
@@ -30,6 +31,9 @@ public class ExportFileFactory {
 	
 	@Autowired
 	ExportFileStockManufacturer exportFileStockManufacturer;
+	
+	@Autowired
+	ExportFileStockCeirAdmin exportFileStockCeirAdmin;
 
 	public ExportFile getObject(String userType, int featureId) {
 		
@@ -47,6 +51,9 @@ public class ExportFileFactory {
 				return exportFileStockCustom;
 			case "MANUFACTURER":
 				return exportFileStockManufacturer;
+			case "CEIRADMIN":
+				return exportFileStockCeirAdmin;
+				
 			default:
 				break;
 			}
