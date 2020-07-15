69aea89df3d536792f1293a3748e5891d871c42d
user <user@user-PC>
2020-07-15 10:22:12 +0530

    403 error resolved

--- CeirPannelCode/src/main/webapp/resources/project_js/searchUserName.js
+++ CeirPannelCode/src/main/webapp/resources/project_js/searchUserName.js
@@ -17,7 +17,7 @@
 			var searchRequest={
 					"username" : $('#Search').val()
 			}
-			//////console.log("searchRequest--->" +JSON.stringify(searchRequest));
+			//console.log("searchRequest--->" +JSON.stringify(searchRequest));
 			var token = $("meta[name='_csrf']").attr("content");
 			var header = $("meta[name='_csrf_header']").attr("content");
 			$.ajaxSetup({
