86b291f299138fb0745a89de42e7d1a6db2f35d1
mehtagit <mehtagit>
2020-08-05 15:49:49 +0530

    aug5

--- SLAModule/src/main/java/com/ceir/SLAModule/App.java
+++ SLAModule/src/main/java/com/ceir/SLAModule/App.java
@@ -16,12 +16,11 @@ import org.slf4j.LoggerFactory;
 public class App 
 {
 	private final static Logger log =LoggerFactory.getLogger(App.class);
-	
 	public static void main(String[] args) {
-		
 	    	ConfigurableApplicationContext	ctx =SpringApplication.run(App.class, args);
 	    	SLAService sLAService=ctx.getBean(SLAService.class);
 			new Thread(sLAService).start();	 
 			}
 	    }
+
 
