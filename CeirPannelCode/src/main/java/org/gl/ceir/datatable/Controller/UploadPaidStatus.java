34bbaca3d54b5cfe1bb850b8d3c74d38135b85c6
user <user@user-PC>
2020-08-10 10:11:10 +0530

    Sharad,Jiten Code Merged on Saturday

--- CeirPannelCode/src/main/java/org/gl/ceir/datatable/Controller/UploadPaidStatus.java
+++ CeirPannelCode/src/main/java/org/gl/ceir/datatable/Controller/UploadPaidStatus.java
@@ -90,6 +90,7 @@ public class UploadPaidStatus {
 		  request.setFeatureId(12);
 		  request.setNid(nid);
 		 request.setUserTypeId(17);
+		 request.setUsername("NA");
 		  request.setUserType("End User");
 		  
 		GenricResponse response = uploadPaidStatusFeignClient.respone(request);
