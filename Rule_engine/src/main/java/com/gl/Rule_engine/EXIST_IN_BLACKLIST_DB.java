6baf1b87115d0a79e954da0fa480d0ad180eda85
mehtagit <mehtagit>
2020-08-20 12:18:11 +0530

    aug

--- Rule_engine/src/main/java/com/gl/Rule_engine/EXIST_IN_BLACKLIST_DB.java
+++ Rule_engine/src/main/java/com/gl/Rule_engine/EXIST_IN_BLACKLIST_DB.java
@@ -74,7 +74,7 @@ public class EXIST_IN_BLACKLIST_DB {
                     case "Reject": {
                          logger.debug("Action is Reject");
 
-                         String fileString = args[15] + " , Error Description : IMEI/ESN/MEID is already present in the system  ";
+                         String fileString = args[1] + " , Error Description : IMEI/ESN/MEID is already present in the system  ";
 
                          bw.write(fileString);
                          bw.newLine();
