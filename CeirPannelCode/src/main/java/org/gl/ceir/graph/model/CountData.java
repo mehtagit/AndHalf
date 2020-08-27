b885f49d4151864d96758aff769ec1e5724b8341
user <user@user-PC>
2020-08-27 19:15:10 +0530

    commit: active Device graph

--- CeirPannelCode/src/main/java/org/gl/ceir/graph/model/CountData.java
+++ CeirPannelCode/src/main/java/org/gl/ceir/graph/model/CountData.java
@@ -31,6 +31,61 @@ public class CountData {
 	@SerializedName("Rejected TAC")
 	private String rejectedTAC;
 
+	
+	
+	@JsonProperty("Total Grey IMEI Count")
+	@SerializedName("Total Grey IMEI Count")
+	private String totalGreyIMEI;
+	
+	@JsonProperty("Total BlackList IMEI Count")
+	@SerializedName("Total BlackList IMEI Count")
+	private String totalBlackListIMEI;
+	
+	
+	@JsonProperty("Total GSMA BlackList Count")
+	@SerializedName("Total GSMA BlackList Count")
+	private String totalGSMABlackList;
+	
+	@JsonProperty("Total Importer Device Count")
+	@SerializedName("Total Importer Device Count")
+	private String totalImporterDevice;
+	
+	@JsonProperty("Total Distributor Device Count")
+	@SerializedName("Total Distributor Device Count")
+	private String totalDistributorDevice;
+	
+	@JsonProperty("Total Retailer Device Count")
+	@SerializedName("Total Retailer Device Count")
+	private String totalRetailerDevice;
+	
+	@JsonProperty("Total Manufacturer Device Count")
+	@SerializedName("Total Manufacturer Device Count")
+	private String totalManufacturerDevice;
+	
+	@JsonProperty("Total End User Device Count")
+	@SerializedName("Total End User Device Count")
+	private String totalEndUserDevice;
+
+	@JsonProperty("Total Stolen Device Count")
+  	@SerializedName("Total Stolen Device Count")
+  	private String totalStolenDeviceCount;
+  	
+  	@JsonProperty("Total Blocked Device Count")
+  	@SerializedName("Total Blocked Device Count")
+  	private String totalBlockedDeviceCount;
+  	
+  	@JsonProperty("Total Tax Paid Device Count")
+  	@SerializedName("Total Tax Paid Device Count")
+  	private String totalTaxPaidDeviceCount;
+     
+  	@JsonProperty("Total Model Count")
+  	@SerializedName("Total Model Count")
+  	private String totalModelCount;
+  	
+  	@JsonProperty("Total Brand Count")
+  	@SerializedName("Total Brand Count")
+  	private String totalBrandCount;
+
 	public String getDate() {
 		return date;
 	}
@@ -79,6 +134,110 @@ public class CountData {
 		this.rejectedTAC = rejectedTAC;
 	}
 
+	public String getTotalGreyIMEI() {
+		return totalGreyIMEI;
+	}
+
+	public void setTotalGreyIMEI(String totalGreyIMEI) {
+		this.totalGreyIMEI = totalGreyIMEI;
+	}
+
+	public String getTotalBlackListIMEI() {
+		return totalBlackListIMEI;
+	}
+
+	public void setTotalBlackListIMEI(String totalBlackListIMEI) {
+		this.totalBlackListIMEI = totalBlackListIMEI;
+	}
+
+	public String getTotalGSMABlackList() {
+		return totalGSMABlackList;
+	}
+
+	public void setTotalGSMABlackList(String totalGSMABlackList) {
+		this.totalGSMABlackList = totalGSMABlackList;
+	}
+
+	public String getTotalImporterDevice() {
+		return totalImporterDevice;
+	}
+
+	public void setTotalImporterDevice(String totalImporterDevice) {
+		this.totalImporterDevice = totalImporterDevice;
+	}
+
+	public String getTotalDistributorDevice() {
+		return totalDistributorDevice;
+	}
+
+	public void setTotalDistributorDevice(String totalDistributorDevice) {
+		this.totalDistributorDevice = totalDistributorDevice;
+	}
+
+	public String getTotalRetailerDevice() {
+		return totalRetailerDevice;
+	}
+
+	public void setTotalRetailerDevice(String totalRetailerDevice) {
+		this.totalRetailerDevice = totalRetailerDevice;
+	}
+
+	public String getTotalManufacturerDevice() {
+		return totalManufacturerDevice;
+	}
+
+	public void setTotalManufacturerDevice(String totalManufacturerDevice) {
+		this.totalManufacturerDevice = totalManufacturerDevice;
+	}
+
+	public String getTotalEndUserDevice() {
+		return totalEndUserDevice;
+	}
+
+	public void setTotalEndUserDevice(String totalEndUserDevice) {
+		this.totalEndUserDevice = totalEndUserDevice;
+	}
+
+	public String getTotalStolenDeviceCount() {
+		return totalStolenDeviceCount;
+	}
+
+	public void setTotalStolenDeviceCount(String totalStolenDeviceCount) {
+		this.totalStolenDeviceCount = totalStolenDeviceCount;
+	}
+
+	public String getTotalBlockedDeviceCount() {
+		return totalBlockedDeviceCount;
+	}
+
+	public void setTotalBlockedDeviceCount(String totalBlockedDeviceCount) {
+		this.totalBlockedDeviceCount = totalBlockedDeviceCount;
+	}
+
+	public String getTotalTaxPaidDeviceCount() {
+		return totalTaxPaidDeviceCount;
+	}
+
+	public void setTotalTaxPaidDeviceCount(String totalTaxPaidDeviceCount) {
+		this.totalTaxPaidDeviceCount = totalTaxPaidDeviceCount;
+	}
+
+	public String getTotalModelCount() {
+		return totalModelCount;
+	}
+
+	public void setTotalModelCount(String totalModelCount) {
+		this.totalModelCount = totalModelCount;
+	}
+
+	public String getTotalBrandCount() {
+		return totalBrandCount;
+	}
+
+	public void setTotalBrandCount(String totalBrandCount) {
+		this.totalBrandCount = totalBrandCount;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -94,8 +253,38 @@ public class CountData {
 		builder.append(approvedTAC);
 		builder.append(", rejectedTAC=");
 		builder.append(rejectedTAC);
+		builder.append(", totalGreyIMEI=");
+		builder.append(totalGreyIMEI);
+		builder.append(", totalBlackListIMEI=");
+		builder.append(totalBlackListIMEI);
+		builder.append(", totalGSMABlackList=");
+		builder.append(totalGSMABlackList);
+		builder.append(", totalImporterDevice=");
+		builder.append(totalImporterDevice);
+		builder.append(", totalDistributorDevice=");
+		builder.append(totalDistributorDevice);
+		builder.append(", totalRetailerDevice=");
+		builder.append(totalRetailerDevice);
+		builder.append(", totalManufacturerDevice=");
+		builder.append(totalManufacturerDevice);
+		builder.append(", totalEndUserDevice=");
+		builder.append(totalEndUserDevice);
+		builder.append(", totalStolenDeviceCount=");
+		builder.append(totalStolenDeviceCount);
+		builder.append(", totalBlockedDeviceCount=");
+		builder.append(totalBlockedDeviceCount);
+		builder.append(", totalTaxPaidDeviceCount=");
+		builder.append(totalTaxPaidDeviceCount);
+		builder.append(", totalModelCount=");
+		builder.append(totalModelCount);
+		builder.append(", totalBrandCount=");
+		builder.append(totalBrandCount);
 		builder.append("]");
 		return builder.toString();
-	} 
+	}
+	
+	
+	
+	
 	
 }
