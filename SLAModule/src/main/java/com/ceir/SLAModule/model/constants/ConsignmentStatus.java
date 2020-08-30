75e9d54564b47a11cd84d30b437584e033f8fe5c
mehtagit <mehtagit>
2020-08-30 13:35:15 +0530

    aug

--- SLAModule/src/main/java/com/ceir/SLAModule/model/constants/ConsignmentStatus.java
+++ SLAModule/src/main/java/com/ceir/SLAModule/model/constants/ConsignmentStatus.java
@@ -3,15 +3,10 @@ package com.ceir.SLAModule.model.constants;
 public enum ConsignmentStatus {
 	
 	INIT(0, "INIT"), PROCESSING(1, "Processing"), REJECTED_BY_SYSTEM(2, "Rejected By System"), 
-	
 	PENDING_APPROVAL_FROM_CEIR_AUTHORITY(3, "Pending Approval From CEIR Authority"), 
-	
 	REJECTED_BY_CEIR_AUTHORITY(4, "Rejected by CEIR Authority"), 
-	
 	PENDING_APPROVAL_FROM_CUSTOMS(5, "Pending Approval From Customs"), APPROVED(6, "Approved"), 
-	
 	REJECTED_BY_CUSTOMS(7, "Rejected By Customs"), WITHDRAWN_BY_IMPORTER(8, "Withdrawn By Importer"),
-	
 	WITHDRAWN_BY_CEIR(9, "Withdrawn By CEIR"), STOLEN(10,"Stolen");
 
 	private int code;
@@ -39,3 +34,4 @@ public enum ConsignmentStatus {
 		return null;
 	}
 }
+
