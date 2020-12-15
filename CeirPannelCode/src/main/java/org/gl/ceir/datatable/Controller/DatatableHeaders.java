30cbefae8f3ece6e8d7f39112c18675bc1bff5bc
user <user@user-PC>
2020-12-15 22:23:15 +0530

    Commit : system country added JS

--- CeirPannelCode/src/main/java/org/gl/ceir/datatable/Controller/DatatableHeaders.java
+++ CeirPannelCode/src/main/java/org/gl/ceir/datatable/Controller/DatatableHeaders.java
@@ -492,6 +492,16 @@ public class DatatableHeaders {
 				}
 				return new ResponseEntity<>(dataTableInputs, HttpStatus.OK);	
 			}
+			
+			//Visa Headers
+			else if("systemAddressHeaders".equals(role)) {
+				
+				String[] headers = {"Province","District","Commune","Villege","table.action"};	
+				for(String header : headers) {
+					dataTableInputs.add(new DatatableHeaderModel(Translator.toLocale(header)));
+				}
+				return new ResponseEntity<>(dataTableInputs, HttpStatus.OK);	
+			}
 
 			//DEFAULT PORTION  
 			else {
