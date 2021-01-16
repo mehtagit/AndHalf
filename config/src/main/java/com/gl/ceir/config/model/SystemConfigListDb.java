2744591815630bcabdd5f4269d4a07269393bf08
user <user@172.24.13.80>
2021-01-16 20:48:45 +0530

    commit: ViewAll icon related API

--- config/src/main/java/com/gl/ceir/config/model/SystemConfigListDb.java
+++ config/src/main/java/com/gl/ceir/config/model/SystemConfigListDb.java
@@ -2,7 +2,6 @@ package com.gl.ceir.config.model;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
